f30f595ade8f09e96d2c2e6782b9b86436c83629
jfschaefer <jan.frederik.schaefer@fau.de>
2025-12-05 17:12:13 +0100

    support enumerations in coverage report

--- flexi/test/magma_coverage/coverage_report.py
+++ flexi/test/magma_coverage/coverage_report.py
@@ -1,4 +1,6 @@
+import re
 from pathlib import Path
+from typing import Literal, Iterable
 
 # only the first N paragraphs from each file are considered
 NUMBER_OF_PARAGRAPHS = 1000
@@ -64,46 +66,83 @@ def process_sentence_pgf(sentence):
     results = concrete.parse(sentence_preprocess(sentence), n=100)
     return list(results)
 
+
+OUTCOME = Literal['NONE', 'TOO MANY'] | int
+
+
+def process_sentence(sentence: str | list, is_train, separator, in_enum: bool = False) -> Iterable[OUTCOME]:
+    if isinstance(sentence, list):
+        is_plain_enum = all(isinstance(s, list) for s in sentence)
+        for s in sentence:
+            if isinstance(s, list):
+                for ss in s:
+                    yield from process_sentence(ss, is_train, separator + f'\033[48;2;255;0;255m+\033[0m', in_enum=True)
+        if is_plain_enum:
+            return
+        else:  # enum is embedded in sentence
+            sentence_actual = ' '.join(
+                item if isinstance(item, str) else 'STATEMENT_ENUM_PLACEHOLDER'
+                for item in sentence
+            )
+    else:
+        sentence_actual = sentence
+
+    if in_enum:  # potentially remove enumeration marker
+        parts = [
+            r'•',
+            r'(\(?[0-9][0-9.]*\)?)',
+            r'\(?[a-z]\)',
+            r'((i|ii|iii|iv|v|vi|vii|viii|ix|x|xi|xii)\.)',
+            r'(\(?(i|ii|iii|iv|v|vi|vii|viii|ix|x|xi|xii)\))',
+            ]
+        for part in parts:  # iirc, '|' is not commutative in re, and I don't want to deal with that :D
+            if m := re.match('^(' + part + ') ', sentence_actual):
+                sentence_actual = sentence_actual[len(m.group(0)):].lstrip()
+                break
+
+    print(separator)
+
+    print(f'Sentence: \033[48;2;255;255;0m{sentence_actual}\033[0m')
+    try:
+        number = len(process_sentence_pgf(sentence_actual))
+        if number < 100:
+            if is_train:
+                print('\033[30;42m' + f'OK: {number} readings' + '\033[0m')
+            yield number
+        else:
+            if is_train:
+                print('\033[30;41m' + f'ERROR: more than 100 readings' + '\033[0m')
+            yield 'TOO MANY'
+    except pgf.ParseError as e:
+        if is_train or 'Unexpected token' in str(e):
+            print('\033[30;41m' + f'ERROR: {e}' + '\033[0m')
+        yield 'NONE'
+
+
 def main():
-    ok = {}
-    total = {}
+    results: dict[Path, list[OUTCOME]] = {}
     for file in COVERAGE_FILES:
         is_train = 'train' in file.name
-        total[file] = 0
-        ok[file] = 0
+        results[file] = []
         print(f'Processing {file}...')
+
         with file.open() as f:
             paragraphs = json.load(f)
+
         for paranum, para in enumerate(paragraphs[:NUMBER_OF_PARAGRAPHS]):
             print(f'Processing {para["paper"]}')
             for sentence in para['sentences']:
-                total[file] += 1
-                print('---', '' if is_train else '\033[48;2;255;0;255m', file.name, f'({paranum}) \033[0m')
-                print(f'Sentence: \033[48;2;255;255;0m{sentence}\033[0m')
-                try:
-                    number = len(process_sentence_pgf(sentence))
-                    if number < 100:
-                        if is_train:
-                            print('\033[30;42m' + f'OK: {number} readings' + '\033[0m')
-                        ok[file] += 1
-                    else:
-                        if is_train:
-                            print('\033[30;41m' + f'ERROR: more than 100 readings' + '\033[0m')
-                except pgf.ParseError as e:
-                    if is_train or 'Unexpected token' in str(e):
-                        print('\033[30;41m' + f'ERROR: {e}' + '\033[0m')
-
-                # shell_output = process_sentence_shell(sentence)
-                # if shell_output.startswith('The parser failed at token') or \
-                #         shell_output.startswith('The sentence is not complete'):
-                #     print('ERROR:', shell_output)
-                # else:
-                #     ok[file] += 1
-                #     print('OK:', len(shell_output.splitlines()), 'readings')
+                results[file].extend(process_sentence(
+                    sentence,
+                    is_train,
+                    separator = '---' + ('' if is_train else '\033[48;2;255;0;255m') + ' ' + file.name + ' ' + f'({paranum}) \033[0m'
+                ))
 
     print('Coverage results:')
     for file in COVERAGE_FILES:
-        print(f'{file.name}: {ok[file]} / {total[file]} = {ok[file] / total[file] * 100:.2f} %')
+        ok = sum(1 for r in results[file] if isinstance(r, int))
+        total = len(results[file])
+        print(f'{file.name}: {ok} / {total} = {ok / total * 100:.2f} %')
 
 
 if __name__ == "__main__":
